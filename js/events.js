abf39e68b12ea3ba3483809c376c7a22735b4349
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-23 19:48:40 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
-$(document).ready(function(){
-
-// call functions here
-
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  pressIt();
+  sunbitIt();
 });
+
+
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+ $("form").on("keydown",function(key){
+   if (key.which==71){
+     alert('g was pressed')
+   }
+ })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+  return alert("Your form is going to be submitted now.")
+  })
+}
