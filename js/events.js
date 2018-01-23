995d00f16a1785ccaa1b786a2757a924649524a1
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-23 19:12:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
 //define functions here
 
-$(document).ready(function(){
-
-// call functions here
-
+$(document).ready(function() {
+  getIt();
+  frameIt();
 });
+
+
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass('tasty')
+  })
+}
+function pressIt(){
+
+}
